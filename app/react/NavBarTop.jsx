63d192f686704cf027af64390e699d789499cb00
Chris Umbach <chrisumbach@gmail.com>
2018-01-14 09:22:02 -0800

    link on about

--- app/react/NavBarTop.jsx
+++ app/react/NavBarTop.jsx
@@ -7,9 +7,9 @@ export default class NavBarTop extends Component {
 
   render() {
       return (
-        <div onClick={this.props.setUpCanvas} className="nav-bar-top">
-            <div className="nav-bar-title">CryptoCanvas</div>
-            <div className="nav-bar-about">About</div>
+        <div className="nav-bar-top">
+            <div onClick={this.props.setUpCanvas} className="nav-bar-title">CryptoCanvas</div>
+            <a href="https://github.com/cumbach/CryptoCanvas" target="_blank"><div className="nav-bar-about">About</div></a>
         </div>
       )
     }
