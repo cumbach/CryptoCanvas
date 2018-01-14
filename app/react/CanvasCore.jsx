34498b304f47dd1e4af9c0a6819de9c5719ce3cf
Eihcir0 <eihcir0@gmail.com>
2018-01-13 16:32:11 -0800

    Add babel polyfill for obj spread operator

--- app/react/CanvasCore.jsx
+++ app/react/CanvasCore.jsx
@@ -3,10 +3,13 @@ import React, { Component } from 'react'
 import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 
-import App from './App.jsx'
+import App from './app/react/App.jsx'
 
 // Import our contract artifacts and turn them into usable abstractions.
 import canvas_artifacts from './../../build/contracts/CanvasCore.json'
+
+const TOTAL_PIXEL_COUNT = 100
+const ASSUMED_INITIALLY_PURCHASED_PIXELS = 3
 
 
 export default class CanvasCore extends Component {
@@ -26,18 +29,6 @@ export default class CanvasCore extends Component {
         this.startUp()
     }
 
-    componentDidMount() {
-        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-        if (typeof web3 !== 'undefined') {
-           console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-            //  Use Mist/MetaMask's provider
-            this.web3 = new Web3(web3.currentProvider);
-        } else {
-            console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-            // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-            this.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-        }
-    }
 
     startUp() {
         const self = this
@@ -65,42 +56,30 @@ export default class CanvasCore extends Component {
         })
     }
 
+    testCode() {
+        this.getPrice();
+    }
+
+    getAllPixels() {
+        return [
+            {
+                id: 27,
+                color: 0,
+                link: 'https://www.google.com',
+                comment: 'hi mom',
+                owner: 'addressowner1',
+                price: 27, // ?Fraction of Ether?
+                coolDownTime: 200, //hours
+            }
+        ]
+    }
+
+
+    //ALL CODE BELOW HERE RE: METACOIN
     setStatus(status) {
         this.setState({ status })
     }
 
-    // refreshBalance() {
-    //     const { account } = this.state
-    //     var meta
-    //     this.CanvasCore.deployed().then(instance => {
-    //         meta = instance
-    //         return meta.getBalance.call(account, { from: account });
-    //     }).then(value => {
-    //         this.setState({ balance: value.valueOf() })
-    //     }).catch(e => {
-    //         console.log(e);
-    //         this.setStatus("Error getting balance; see log.");
-    //     });
-    // }
-    //
-    // sendCoin() {
-    //     const { account, amount, receiver } = this.state
-    //
-    //     this.setStatus("Initiating transaction... (please wait)");
-    //
-    //     var meta
-    //     var self = this
-    //     this.CanvasCore.deployed().then(instance => {
-    //         meta = instance
-    //         return meta.sendCoin(receiver, parseInt(amount), { from: account });
-    //     }).then(() => {
-    //         self.setStatus("Transaction complete!");
-    //         self.refreshBalance();
-    //     }).catch(e => {
-    //         console.log(e);
-    //         self.setStatus("Error sending coin; see log.");
-    //     })
-    // }
     getPrice() {
       console.log('getPrice');
       this.CanvasCore.deployed().then(instance => {
@@ -109,11 +88,6 @@ export default class CanvasCore extends Component {
       }).then(price => {
         console.log(price);
       });
-
-    }
-
-    testCode() {
-      this.getPrice();
 
     }
 
@@ -130,7 +104,54 @@ export default class CanvasCore extends Component {
         } = this.state
 
         return (
-            <App/>
+            <div>
+                <App/>
+                <br/>
+                <br/>
+                <br/>
+                --------------------------------------------------------
+                ^^ Our app above
+                legacy Megacoin render below ~
+                --------------------------------------------------------
+                <h1>CryptoCanvasjsx</h1>
+                <h2>Example Truffle Dapp</h2>
+                <h3>
+                    You have <span className="black"><span id="balance">{this.state.balance}</span> META</span></h3>
+                <br/>
+                <h1>Test Code</h1>
+                <form>
+                    <label>
+                        Amount:
+                    </label>
+                    <input
+                        value={this.state.amount}
+                        onChange={this.handleChange}
+                        type="text"
+                        id="amount"
+                        placeholder="e.g., 95"
+                    />
+                    <br/>
+                    <br/>
+                    <label>
+                        To Address:
+                    </label>
+                    <input
+                        value={this.state.receiver}
+                        onChange={this.handleChange}
+                        type="text"
+                        id="receiver"
+                        placeholder="e.g., 0x93e66d9baea28c17d9fc393b53e3fbdd76899dae"
+                    />
+                </form>
+                <br/><br/>
+                <button id="send" onClick={this.testCode}>
+                    Send MetaCoin
+                </button>
+                <br/><br/>
+                <span id="status"></span>
+                <br/>
+                <span className="hint"><strong>Hint:</strong> open the browser developer console to view any errors and warnings.</span>
+            </div>
         )
     }
 
